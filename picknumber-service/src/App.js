9a6bd81b2fcd202264ef240b11937c4b7a3ecfa9
dlgywls1130 <dlgywls1130@gmail.com>
2023-05-01 21:41:11 +0900

    login page

--- picknumber-service/src/App.js
+++ picknumber-service/src/App.js
@@ -1,10 +1,10 @@
-
-import './App.css';
+import './App.css'
+import Login from './components/Login'
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Login/>
     </div>
   );
 }
